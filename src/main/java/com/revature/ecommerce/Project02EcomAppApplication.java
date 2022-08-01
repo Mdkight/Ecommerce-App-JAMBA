98ceddff620864615b9499a51b9ee64ea607b6e3
AngelaHsiao <angelaphsiao@gmail.com>
2022-08-01 11:52:59 -0700

    wireframe

--- src/main/java/com/revature/ecommerce/Project02EcomAppApplication.java
+++ src/main/java/com/revature/ecommerce/Project02EcomAppApplication.java
@@ -2,8 +2,10 @@ package com.revature.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.revature.ecommerce")
 public class Project02EcomAppApplication {
 
 	public static void main(String[] args) {
