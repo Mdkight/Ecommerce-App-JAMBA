345196647a3bcdc44e3bc2842962cb504e769497
AngelaHsiao <angelaphsiao@gmail.com>
2022-08-05 10:04:21 -0700

    add cross origin

--- src/main/java/com/revature/ecommerce/controllers/CustomerController.java
+++ src/main/java/com/revature/ecommerce/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package com.revature.ecommerce.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import com.revature.ecommerce.model.Customer;
 import com.revature.ecommerce.repository.CustomerRepository;
 
 @RestController
+@CrossOrigin(origins ="*")
 public class CustomerController {
 	@Autowired
 	CustomerRepository customerRepository;
