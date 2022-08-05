18097f2ab10d487be92723fafcf17dc90d96c245
Mike Kight <102483977+Mdkight@users.noreply.github.com>
2022-08-05 11:26:47 -0500

    created initial methods for login and account creation

--- src/main/java/com/revature/ecommerce/Project02EcomAppApplication.java
+++ src/main/java/com/revature/ecommerce/Project02EcomAppApplication.java
@@ -3,9 +3,10 @@ package com.revature.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 
 @SpringBootApplication
-//@ComponentScan("com.revature.ecommerce") // that was the whole conversation we just had earlier lol. @SpringBootApplication handles this for us, and defaults to the package that it's in
+@EnableSpringHttpSession
 public class Project02EcomAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/revature/ecommerce/controllers/MainController.java
+++ src/main/java/com/revature/ecommerce/controllers/MainController.java
@@ -3,6 +3,7 @@ package com.revature.ecommerce.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import com.revature.ecommerce.repository.CustomerRepository;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/jamba")
+
 public class MainController {
 	
 	CustomerRepository customerRepository;
@@ -25,6 +27,12 @@ public class MainController {
 	@GetMapping("/login")
 	public ResponseEntity<Customer> loginAttempt(@RequestParam("username") String username, @RequestParam("password") String password){
 		//TODO complete login attempt method here
+		
+	}
+	
+	@PostMapping("/newaccount")
+	public ResponseEntity<Customer> registerNewAccount(@RequestParam("username") String username, @RequestParam("password") String password){
+		
 	}
 	
 }
